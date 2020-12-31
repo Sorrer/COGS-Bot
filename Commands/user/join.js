eac2dff32066ad891c14ff16942b2b6b29da166a
Sorrer <sorrer@gmail.com>
2020-12-30 21:16:36 -0500

    Wrong member name changed

--- Commands/user/join.js
+++ Commands/user/join.js
@@ -83,9 +83,9 @@ module.exports.OnMemberJoin = async function(member){
             let [projectChannels, pFields] = await mysqlCon.query("SELECT * FROM projectchannels WHERE projectid = ?", project.projectid);
 
             for(let pchannel of projectChannels){
-                console.log("Adding them back to extra channel " + pchannel);
+                console.log("Adding them back to extra channel " + pchannel.channelid);
                 let grabbedChannel = server.channels.cache.get(pchannel.channelid);
-                if(grabbedChannel) tools.AddUserToChannel(grabbedChannel, author.id);
+                if(grabbedChannel) tools.AddUserToChannel(grabbedChannel, member.id);
             }
         }
     }
@@ -111,9 +111,9 @@ module.exports.OnMemberJoin = async function(member){
             let [projectChannels, pFields] = await mysqlCon.query("SELECT * FROM projectchannels WHERE projectid = ?", project[0].projectid);
 
             for(let pchannel of projectChannels){
-                console.log("Adding them back to extra channel " + pchannel);
+                console.log("Adding them back to extra channel " + pchannel.channelid);
                 let grabbedChannel = server.channels.cache.get(pchannel.channelid);
-                if(grabbedChannel) tools.AddUserToChannel(grabbedChannel, author.id);
+                if(grabbedChannel) tools.AddUserToChannel(grabbedChannel, member.id);
             }
         }
     }
