ebf9c53d586a5146a7b084418a9851a81d2009d8
Sorrer <sorrer@gmail.com>
2021-01-04 00:20:31 -0500

    Create MysqlHandler functionality

---
+++ CogsLib/MysqlHandler.js
@@ -0,0 +1,38 @@
+// Class pass MYSQL distribution.
+// Used to provide mysql connection access to different commands as needed.
+// Can create quick query functions for popular calls. For example, getting a project from a specific server.
+
+
+const mysql = require('mysql2');
+
+
+class MysqlHandler {
+
+	constructor(userInfo, logger) {
+		this.userInfo = userInfo;
+		this.logger = logger;
+
+		if(!logger) {
+			this.logger.local = console.log;
+		}
+	}
+
+
+	async init() {
+		this.connection = await mysql.createPool(this.userInfo).promise();
+	}
+
+
+	async query(query, values, within = false) {
+		if(within !== true) {
+			this.logger.localDebug('Executed non static query. Please use a default method.\n' + query);
+		}
+
+		const [results, fields] = await this.connection.query(query, values);
+		return { results: results, fields: fields };
+	}
+
+}
+
+
+module.exports = MysqlHandler;
