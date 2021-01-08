916135439208b9cf14fe843f3d1aa8d961bf5bc5
Sorrer <sorrer@gmail.com>
2021-01-07 23:39:24 -0500

    Added test server definer command

---
+++ Commands/botadmin/SetTestServer.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+	description: {
+
+		command: 'settestserver',
+		name: 'SetTestServer',
+		description: 'Sets the current server to the test server',
+		usage: 'setprefix <prefix> - Prefix to use before commands',
+		category: 'Bot Admin'
+
+	},
+
+	settings: {
+
+		enabled: true,
+		isTask: false,
+		allowedUsage: ['botadmin'],
+		requireProjectOwner: false,
+		requiredParams: 0,
+		allowDM: false,
+		onlyTestServer: false
+
+	},
+
+	execute: async function(data) {
+		data.cache.setSetting('istestserver', 1);
+		data.message.reply('Server is now registered as a test server! Have fun debugging test commands');
+
+		data.cache.logger.local('Warning! New test server set.' + ' <@' + data.message.author.id + '> set the server as a test server');
+	}
+};
