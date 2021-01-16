5e9c7cedbb3d705732477b4461918fbd9d480837
Sorrer <sorrer@gmail.com>
2021-01-16 00:47:05 -0500

    Added category search in help command

--- Commands/basic/Help.js
+++ Commands/basic/Help.js
@@ -39,8 +39,39 @@ module.exports = {
 				}
 			}
 
+
 			if(commandName === false) {
-				const embededmsg = data.cache.logger.generateMsg('Help - ' + data.params[0] + ' Command', 'Command not found', '#b8002f');
+				// TODO: If no command is found, check for category
+
+				for(const category in data.commandscategorized) {
+					if(category == data.params[0]) {
+
+						let helpCommandsString = '';
+						for(const command of data.commandscategorized[category]) {
+							if(command.settings.privilege <= data.userdata.privilege) {
+								if(command.settings.onlyTestServer && (data.cache.getSetting && !data.cache.getSetting('istestserver'))) {
+									continue;
+								}
+								helpCommandsString += '> ' + data.cache.prefix + command.description.usage + '\n';
+								// console.log(helpCommandsString);
+							}
+						}
+
+						if(category != '' && category != null && category != undefined && category != 'undefined') {
+							if(helpCommandsString != '') {
+								helpString += '**' + category + '**\n';
+							}
+						}
+						helpString += helpCommandsString;
+
+						const embededmsg = data.cache.logger.generateMsg('Help - ' + data.params[0], helpString, '#88519e');
+						data.message.channel.send(embededmsg);
+
+						return;
+					}
+				}
+
+				const embededmsg = data.cache.logger.generateMsg('Help - ' + data.params[0] + ' Command/Category', 'Command/Category not found', '#b8002f');
 
 				data.message.channel.send(embededmsg);
 
@@ -53,7 +84,6 @@ module.exports = {
 
 			return;
 
-			// TODO: If no command is found, check for category
 		}
 
 		let helpString = '';
